6b6d2f79ca0a69dfe3fa9e0d60690cfa6bb86d15
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-05-17 16:20:30 +0900

    ActionCableを使用したチャットアプリの実装

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,40 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+    console.log('connected')
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(message) {
+    // Called when there's incoming data on the websocket for this channel
+    
+    // viewのidがmessagesのオブジェクトを取得
+    const messages = document.getElementById('messages')
+    // オブジェクトのHTMLに以下のデータを追加で挿入
+    messages.innerHTML += `<p>${message}</p>`
+  },
+
+  speak: function(content) {
+    // messageに引数のcontentで受け取った値を渡す
+    return this.perform('speak', {message: content});
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // rooms/shou.html.erbのidがchat-inputのオブジェクトを取得
+  const input = document.getElementById('chat-input')
+  // room/shou.html.erbのidがbuttonのオブジェクトを取得
+  const button = document.getElementById('button')
+  // buttonクリック時の操作を記述
+  button.addEventListener('click', function() {
+    // inputに入力された値を取得
+    const content = input.value
+    // App.roomのspeakメソッドを実行
+    App.room.speak(content)
+    // inputの入力値を空文字にする
+    input.value = ''
+  })
+})
---
+++ app/channels/room_channel.rb
@@ -0,0 +1,18 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+  
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+  
+  def speak(data)
+    # オブジェクトをmessageに格納
+    message = Message.create!(content: data['message'])
+    # _message.html.erbにデータを渡してHTMLを生成
+    template = ApplicationController.renderer.render(partial: 'messages/message', locals: {message: message})
+    # 生成されたHTMLを渡してbroadcast
+    ActionCable.server.broadcast 'room_channel', template
+  end
+end
